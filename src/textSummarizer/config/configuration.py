222393bb654e7cd078b178a27824e35a085617ba
Devi-Suhitha <suhithareddykesari@gmail.com>
2026-07-05 18:19:01 +0530

    model evaluation completed

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,7 +1,13 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
 
-from textSummarizer.entity import DataIngestionConfig, DataValidationConfig,DataTransformationConfig
+from textSummarizer.entity import (
+    DataIngestionConfig,
+    DataValidationConfig,
+    DataTransformationConfig,
+    ModelEvaluationConfig,
+    ModelTrainerConfig,
+)
 
 
 class ConfigurationManager:
@@ -16,6 +22,9 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
+    # ======================================================
+    # Data Ingestion
+    # ======================================================
     def get_data_ingestion_config(self) -> DataIngestionConfig:
 
         config = self.config.data_ingestion
@@ -31,6 +40,9 @@ class ConfigurationManager:
 
         return data_ingestion_config
 
+    # ======================================================
+    # Data Validation
+    # ======================================================
     def get_data_validation_config(self) -> DataValidationConfig:
 
         config = self.config.data_validation
@@ -46,6 +58,9 @@ class ConfigurationManager:
 
         return data_validation_config
 
+    
+    # Data Transformation
+    
     def get_data_transformation_config(self) -> DataTransformationConfig:
 
         config = self.config.data_transformation
@@ -55,7 +70,53 @@ class ConfigurationManager:
         data_transformation_config = DataTransformationConfig(
             root_dir=config.root_dir,
             data_path=config.data_path,
-            tokenizer_name=config.tokenizer_name
+            tokenizer_name=config.tokenizer_name,
         )
 
         return data_transformation_config
+
+    
+    # Model Trainer
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+
+        config = self.config.model_trainer
+        params = self.params.TrainingArguments
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_ckpt=config.model_ckpt,
+            num_train_epochs=params.num_train_epochs,
+            warmup_steps=params.warmup_steps,
+            per_device_train_batch_size=params.per_device_train_batch_size,
+            weight_decay=params.weight_decay,
+            logging_steps=params.logging_steps,
+            evaluation_strategy=params.evaluation_strategy,
+            eval_steps=params.eval_steps,
+            save_steps=params.save_steps,
+            gradient_accumulation_steps=params.gradient_accumulation_steps,
+        )
+
+        return model_trainer_config
+    
+    
+    
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_path = config.model_path,
+            tokenizer_path = config.tokenizer_path,
+            metric_file_name = config.metric_file_name
+           
+        )
+
+        return model_evaluation_config
